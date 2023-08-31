7f25fe2718753b1757a50642de01511ceeb784ac
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-31 13:38:49 +0200

    Migrate pure Redux to RTK. Cart(1).MIGRATIONS DONE

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,27 +1,25 @@
-import { useDispatch, useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from "react-redux"
 import {
   selectCartCount,
   selectIsCartOpen,
-} from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.action';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+} from "../../store/cart/cart.selector"
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
+import { CartIconContainer, ItemCount } from "./cart-icon.styles"
+import { setIsCartOpen } from "../../store/cart/cart.reducer"
 
 const CartIcon = () => {
-  const dispatch = useDispatch();
-  const isCartOpen = useSelector(selectIsCartOpen);
-  const cartCount = useSelector(selectCartCount);
+  const dispatch = useDispatch()
+  const isCartOpen = useSelector(selectIsCartOpen)
+  const cartCount = useSelector(selectCartCount)
 
-  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon' />
+      <ShoppingIcon className="shopping-icon" />
       <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
-  );
-};
+  )
+}
 
-export default CartIcon;
+export default CartIcon
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -12,11 +12,13 @@ export const userSlice = createSlice({
   initialState: INITIAL_STATE,
   reducers: {
     setCurrentUser(state, action) {
-      console.log("USER")
       state.currentUser = action.payload // This looks like a mutation, but it is not. Under the hood Redux generates a new object, because
       // RTK leverages a library called Immer than ensures the inmutability.Remember: Redux is based on the INMUTABILITY OF STATES.
       //It generates a new object each time an action is passed trought it.
       //This will generate the action {type: user/setCurrentUser, payload: ...} which will be dispatched in App.js
+      // IMPORTANT: The ENTIRE store continues reducing with each individual action !!!!!!! (as in pure Redux). So, createSelectors in order to
+      // cache values still have sense to avoid useless re-renderings with each dispatched action on all the components that are consuming
+      // the store with useSelector.
     },
   },
 })
