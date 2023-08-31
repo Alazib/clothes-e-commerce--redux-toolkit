1524a50d9db31c85f8645c9f98e5efe3506a8dff
J.Alberto Azevedo <alazib7@gmail.com>
2023-08-31 14:09:02 +0200

    Add a thunk to categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,15 @@
 import { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import { useDispatch } from "react-redux"
-
 import CategoriesPreview from "../categories-preview/categories-preview.component"
 import Category from "../category/category.component"
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"
-import { setCategories } from "../../store/categories/category.reducer"
+import { fetchCategoriesThunk } from "../../store/categories/category.reducer"
 
 const Shop = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments("categories")
-      dispatch(setCategories(categoriesArray))
-    }
-
-    getCategoriesMap()
+    dispatch(fetchCategoriesThunk())
   }, [])
 
   return (
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,17 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 }
+
+export const fetchCategoriesThunk = createAsyncThunk(
+  "categories/fetchFromFirestore",
+  async () => {
+    const categories = await getCategoriesAndDocuments()
+    return categories
+  }
+)
 
 export const categoriesSlice = createSlice({
   name: "categories",
   initialState: CATEGORIES_INITIAL_STATE,
-  reducers: {
-    setCategories(state, action) {
-      console.log("CATEGORIES")
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategoriesThunk.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchCategoriesThunk.fulfilled, (state, action) => {
       state.categories = action.payload
-    },
+      state.isLoading = false
+    })
+    builder.addCase(fetchCategoriesThunk.rejected, (state, action) => {
+      state.error = action.error
+    })
   },
 })
 
